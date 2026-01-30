089ddc2d983484c1713c7f3c75944504d4ae62fb
Dwortham9 <dwortham9@gmail.com>
2026-01-30 14:47:33 -0600

    Add files via upload

---
+++ java-scripts/copy_array_new/Homework5B.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Homework5B
+{
+
+    public static void main(String[] args) 
+    {
+        int[] num1 = new int[] {7,3,1,1,5};
+        System.out.println("Num 1");
+        for (int i = 0; i < num1.length; i++)
+            System.out.print(num1[i]);
+        
+        System.out.println(" ");
+        
+        int[] num2 = Arrays.copyOf(num1, 5);
+        System.out.println("Num 2");
+        for (int i = 0; i < num2.length; i++)
+            System.out.print(num2[i]);
+    }
+    
+}
