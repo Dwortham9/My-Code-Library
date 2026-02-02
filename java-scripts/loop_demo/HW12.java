72e07b1b181ca9acf8e89e90e71bfbdfdb81e45e
Dejenna Wortham <dwortham9@gmail.com>
2026-02-02 09:48:49 -0600

    Add files via upload

---
+++ java-scripts/loop_demo/HW12.java
@@ -0,0 +1,31 @@
+
+public class HW12
+{
+    public static void main(String[] args) 
+    {
+        int i = 1;
+        System.out.print("While loop: ");
+        while (i <= 5) 
+        {
+          System.out.print(i+" ");
+          i++;
+        }
+        System.out.println(" ");
+        System.out.print("For loop: ");
+        for (int x = 6; x <= 10; x++) 
+        {
+            System.out.print(x+" ");
+        }
+        System.out.println(" ");
+        System.out.print("Do/While loop: ");
+        int y = 11;
+        do 
+        {
+          System.out.print(y+" ");
+          y++;
+        }
+        while (y <= 15);
+        System.out.println(" ");
+    }
+    
+}
