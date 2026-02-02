5f8b5c1c70d50f487f3c3903cc14280728dcf088
Dejenna Wortham <dwortham9@gmail.com>
2026-02-02 09:36:16 -0600

    Add files via upload

---
+++ java-scripts/MathDemo/HW7pt1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class HW7pt1
+{
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a whole number > ");
+        int n = sc.nextInt();
+        displayTwiceTheNumber(n);
+        displayNumberPlusFive(n);
+        displayNumberSquared(n);
+         
+    }
+    public static void displayTwiceTheNumber(int n) {
+        int result = n * 2;
+        System.out.printf("%d * 2 = %d\n", n, result);
+    }
+    public static void displayNumberPlusFive(int n) {
+        int result = n+5;
+        System.out.printf("%d + 5 = %d\n", n, result);
+    }
+    public static void displayNumberSquared(int n) {
+      int result = n*n;
+      System.out.printf("%d squared = %d\n", n, result);
+   }
+    
+}
