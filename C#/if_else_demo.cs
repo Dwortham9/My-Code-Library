40ad6433eff3ba4b7431c6bba1c0e3c442e0fd41
Dejenna Wortham <dwortham9@gmail.com>
2026-02-06 15:33:04 -0600

    Update if_else_demo.cs

--- C#/if_else_demo.cs
+++ C#/if_else_demo.cs
@@ -3,3 +3,9 @@ if (e < f || f < 0)
     Console.Write("Purple");
 else
     Console.Write("Gold");
+
+﻿int e = 5, f = 10;
+if (e < f && f < 0)
+    Console.Write("Purple");
+else
+    Console.Write("Gold");
